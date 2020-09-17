b854f4832d6d57b29d972c2bd71870e6a475fdf0
Youhyun <1486asdf@gmail.com>
2020-09-17 14:34:48 +0000

    change to n>10

--- main.c
+++ main.c
@@ -3,19 +3,18 @@
 #include <stdlib.h>
 #include <math.h>
 
-int digit_sum(int n);
+int digit_sum(int n){
+  if (n >= 10)
+    return n%10 + digit_sum(n/10);
+  else 
+    return 0;
+}
+
 
 int main(void) {
   int n = atoi(readline("Enter an int: ")); 
-  int num = n;
-  num = (num % 10);
-  printf("sum of digit %d is %d.\n",n , digit_sum(n));
+  printf("sum of digit %d is %d.\n", n , digit_sum(n));
   return 0;
 }
 
-int digit_sum(int n){
-    if (n > 0)
-      return num + digit_sum(n/10);
-    else 
-      return 0;
-}
+
